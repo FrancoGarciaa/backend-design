74c1ec73d9753072c400877457d9b32080ba8fc1
franco <calderonfranco307@gmail.com>
2025-04-05 00:17:58 -0300

    primerrcommit

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,16 +1,16 @@
-import jwt from 'jsonwebtoken';
-import UserModel from '../models/user.model.js';
-import { createHash, isValidPassword } from '../utils/bcrypt.js';
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user.model.js";
+import { createHash, isValidPassword } from "../utils/bcrypt.js";
 
-const JWT_SECRET = 'secretJWT123';
-const JWT_EXPIRES = '1d'; 
+const JWT_SECRET = "secretJWT123";
+const JWT_EXPIRES = "1d"; 
 
 export const registerUser = async (req, res) => {
 try {
     const { first_name, last_name, email, age, password } = req.body;
 
     const exist = await UserModel.findOne({ email });
-    if (exist) return res.status(400).json({ message: 'User already exists' });
+    if (exist) return res.status(400).json({ message: "El usuario ya existe" });
 
     const newUser = new UserModel({
     first_name,
@@ -22,9 +22,9 @@ try {
 
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "Registro exitoso :D" });
 } catch (err) {
-    res.status(500).json({ message: 'Internal error', error: err.message });
+    res.status(500).json({ message: "Error interno", error: err.message });
 }
 };
 
@@ -33,31 +33,31 @@ try {
     const { email, password } = req.body;
 
     const user = await UserModel.findOne({ email });
-    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+    if (!user) return res.status(401).json({ message: "No valido" });
 
     const validPassword = isValidPassword(user, password);
-    if (!validPassword) return res.status(401).json({ message: 'Invalid credentials' });
+    if (!validPassword) return res.status(401).json({ message: "No valido" });
 
     const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, {
     expiresIn: JWT_EXPIRES
     });
 
     res
-    .cookie('token', token, {
+    .cookie("token", token, {
         httpOnly: true,
         secure: false,
-        sameSite: 'strict',
+        sameSite: "strict",
         maxAge: 24 * 60 * 60 * 1000
     })
-    .json({ message: 'Login successful' });
+    .json({ message: "Inicio de sesion exitosa :D" });
 } catch (err) {
-    res.status(500).json({ message: 'Internal error', error: err.message });
+    res.status(500).json({ message: "Error interno", error: err.message });
 }
 };
 
 export const currentUser = (req, res) => {
 if (!req.user) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: "No autorizado" });
 }
 
 const { first_name, last_name, email, age, role } = req.user;
